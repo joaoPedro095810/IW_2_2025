7892a33a7097320d291a6d9a51bb35a97c9bec5a
JoaoFerreira <159969483+joaoPedro095810@users.noreply.github.com>
2025-05-08 09:20:45 -0300

    Delete exercicio01 directory

--- exercicio01/script.js
+++
@@ -1,7 +0,0 @@
-function mostrarMensagem() {
-    alert("JavaScript é divertido");
-}
-
-document.addEventListener("DOMContentLoaded", function()  {
-    document.getElementById("botaoFrase").addEventListener("click", mostrarMensagem);
-})
