cbe2f4f50b286063e661b8bf47a7ee6404a4ffac
JoaoFerreira <159969483+joaoPedro095810@users.noreply.github.com>
2025-05-08 09:20:35 -0300

    Delete exercicio02 directory

--- exercicio02/script.js
+++
@@ -1,7 +0,0 @@
-function atualizarTexto() {
-    document.getElementById("texto").innerText = "texto atualizado com JavaScript";
-  }
-
-document.addEventListener("DOMContentLoaded", function()  {
-    document.getElementById("botãoAtualização").addEventListener("click", atualizarTexto);
-})
